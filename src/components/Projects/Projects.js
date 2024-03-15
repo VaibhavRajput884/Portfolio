8d1630fbba75ae02099118ff8e7f635790851d8d
VaibhavRajput884 <vaibhavr1406@gmail.com>
2024-03-15 11:10:07 +0530

    Final Draft

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.jpg";
+
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Gold Gym"
+              description="A React JS-based fitness exercise application featuring a vast library of workouts, customized training plans, and progress tracking for users seeking an effective fitness regime."
+              ghLink="https://github.com/VaibhavRajput884/Gym-Website"
+              demoLink="https://vercel-gym-plum.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Path Visualizer"
+              description="A visualization tool to display shortest path for different searching algorithms. Built using HTML5  and JavaScript."
+              ghLink="https://github.com/VaibhavRajput884/Path-finder"
+              demoLink="https://pathvisualizationtool.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Route Planner"
+              description="The Route Planning Tool MVP (Minimum Viable Product) is a basic version of the application designed to demonstrate core functionality. It aims to optimize routes for users attending multiple jobs in various locations with minimal features and complexity."
+              ghLink="https://github.com/VaibhavRajput884/Route-Planner-ZTLabs"
+              demoLink="https://route-planner-zt-labs.vercel.app/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Sociopedia"
+              description="Sociopedia is a MERN app that combines social networking with an encyclopedia, allowing users to post text and pictures while engaging in knowledge sharing and community interaction."
+              ghLink="https://github.com/VaibhavRajput884/socially"
+              //demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
