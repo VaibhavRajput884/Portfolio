ab9d4805bb49919d364d27133dc1af5a28d60ee6
VaibhavRajput884 <vaibhav.rajput@jalantechnologies.com>
2024-10-26 12:11:28 +0530

    "Updated resume and about page with new information, formatting, and styling changes."

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -4,35 +4,38 @@ import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className="quote-card-view" style={{ padding: "20px", margin: "20px" }}>
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Vaibhav Rajput </span>
-            from <span className="purple"> Saharanpur, India.</span>
-            <br />
-            I am Final year student at Vellore Institute of Technology.
-            <br />
-            I am pursuing (B.Tech) in Computer Science and engineering
-        
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+          <p style={{ textAlign: "justify", lineHeight: "1.6" }}>
+            Hi Everyone, I am <span className="purple">Vaibhav Rajput</span> 
+            from <span className="purple">Saharanpur, India</span>.
           </p>
-          <ul>
+          <p style={{ textAlign: "justify", lineHeight: "1.6" }}>
+            I graduated with a B.Tech in Computer Science and Engineering from 
+            Vellore Institute of Technology, Bhopal.
+          </p>
+          <p style={{ textAlign: "justify", lineHeight: "1.6" }}>
+            I have 3 months of internship experience at Jalan Technologies, and currently, 
+            I'm working as a Software Engineer Trainee at 
+            <span className="purple"> Volkswagen Group Technology India</span>.
+          </p>
+          <p style={{ textAlign: "justify", lineHeight: "1.6", marginTop: "15px" }}>
+            Besides coding, here are a few activities I love to engage in:
+          </p>
+          <ul style={{ listStyleType: "none", paddingLeft: "0" }}>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
-            
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Traveling
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          <p style={{ color: "rgb(155 126 172)", marginTop: "15px" }}>
+            "Strive to build things that make a difference!"
           </p>
-          <footer className="blockquote-footer">Vaibhav</footer>
+          <footer className="blockquote-footer" style={{ marginTop: "10px" }}>Vaibhav</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-   
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -19,37 +15,23 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Hello! I'm <span className="purple">Vaibhav Rajput</span>, a tech enthusiast from <span className="purple">Saharanpur, India</span>. 
               <br />
-              <br />I am fluent in classics like
+              <br />I developed a deep passion for programming and have explored various languages and frameworks along the way.
+              <br />
+              <br />I am proficient in <b className="purple">Java</b>, <b className="purple">JavaScript</b>, and <b className="purple">React</b>.
+              <br />
+              <br />
+              My main areas of interest include creating innovative 
               <i>
-                <b className="purple"> Java, Javascript and React. </b>
-              </i>
+                <b className="purple"> Web Technologies and Products</b>
+              </i>, 
+              with a special focus on backend development and 
+              modern frameworks.
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b>
-                also in areas related to{" "}
-                <b className="purple">
-                 
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I love bringing ideas to life with technologies like <b className="purple">Node.js</b> and modern JavaScript frameworks like 
+              <b className="purple"> React.js</b> and <b className="purple">Next.js</b>.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -62,7 +44,7 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              I’d love to <span className="purple">connect </span>with you!
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
@@ -70,27 +52,17 @@ function Home2() {
                   href="https://github.com/VaibhavRajput884"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li> */}
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/vaibhav-rajput-623070206/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
@@ -112,4 +84,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
